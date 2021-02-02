0d607034b2030a5575f57f610e37ed753629b14b
martacamacho <martacamachovargas@gmail.com>
2021-02-02 19:53:41 +0100

    nivell 1 done

---
+++ js/login.js
@@ -0,0 +1,17 @@
+/* NIVELL 1 */
+
+function showOnConsoleEmail() {
+    var email= document.getElementById("email").value;
+    console.log('Este es el mail: ' + email);
+}
+
+function showOnConsoleName() {
+  var name= document.getElementById("name").value;
+  console.log('Este es el nombre: ' + name);
+}
+
+function showOnConsoleSurname() {
+  var surname= document.getElementById("surname").value;
+  console.log('Este es el apellido: ' + surname);
+}
+
